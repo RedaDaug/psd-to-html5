087d99729b989891800e3c323f66beb4ba7b49d6
Reda <reda.daugelaviciute@gmail.com>
2018-11-29 18:56:23 +0100

    browser sync added

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,24 @@ postcss = require('gulp-postcss'),
 cssVars = require('postcss-simple-vars'),
 autoprefixer = require('autoprefixer'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 
 gulp.task('styles', function(){
 	return gulp.src('./css/style.css')
 		.pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
-		.pipe(gulp.dest('./temp/style.css'));	
+		.pipe(gulp.dest('./temp/style.css'));
+	
 });
 
 gulp.task('watch', function(){
+	
+	browserSync.init({
+		server: {
+			baseDir: "."
+		}
+	});
 	
 	watch('./css/**/*.css', function(){
 		gulp.start('styles');
