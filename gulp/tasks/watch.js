0dd916ceadfa88e95ec1bf57678ee68df59896b0
Reda <reda.daugelaviciute@gmail.com>
2018-12-07 12:52:01 +0100

    iPohne slides in

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+cssVars = require('postcss-simple-vars'),
+autoprefixer = require('autoprefixer'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
+
+
+gulp.task('styles', function(){
+	return gulp.src('./css/style.css')
+		.pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
+		.pipe(gulp.dest('./temp'));
+	
+});
+
+
+gulp.task('watch', function(){
+	
+	browserSync.init({
+		server: {
+			baseDir: "."
+		}
+	});
+	
+	watch('./css/**/*.css', function(){
+		gulp.start('styles');
+	});
+	
+	
+});
